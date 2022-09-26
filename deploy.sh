563c129ca78dabf45bc8cb2b01c826c8f3742a00
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2022-09-26 15:13:43 +0200

    Revert "Debug"
    This reverts commit 859c42b2eef6e0539ed5f95ccecf6956df6e8b15.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-set -ex
+set -e
 echo "Getting token from Github"
 TOKEN=$(curl -H "Authorization: bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL" --silent | jq -r '.value')
 echo "Deploying new version"
 ROLLOUT_ID=$(curl -H "Authorization:Bearer $TOKEN" "$ENDPOINT/github/deploy/$FEATURE_NAME" -X POST -d "$JSON" --fail --silent| jq -r '.rollout')
-echo "Rollout done with id $ROLLOUT_ID"
 echo '### Rollout created! :rocket:' >> "$GITHUB_STEP_SUMMARY"
 echo "[Rollout progress](https://fasit.nais.io/rollout/$ROLLOUT_ID)" >> "$GITHUB_STEP_SUMMARY"
